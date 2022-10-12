92d62211728ea9bba9eda4616e1f3dec27f6c630
Christiaan Visagie <visagiechristiaan40@gmail.com>
2022-10-12 22:16:42 +0200

    Dynamic links added

--- admin/Includes/collapseMenu.php
+++ admin/Includes/collapseMenu.php
@@ -1,15 +1,37 @@
 <?php
     function makeSubMenu($name){
+        $icon = "<span><i class='fa-solid fa-chevron-circle-right blu-text'></i></span>";
+        $liStyle = "style='background-color:transparent; margin-left:15px; text-decoration:none;'";
+        $aStyle = "style='text-decoration:none; color:#FFF !important;'";
+    
         if($name == "BiClass"){
+            $link = getLink("BiClass");
             echo "<div class='collapse multi-collapse' id='multiCollapse_$name'>
                     <div>
-                        <ul>
-                            <li>
-                                <a href='' class=''>Settings</a>
+                        <ul class='fa-ul'>
+                            <li $liStyle>
+                                $icon
+                                <a href='$link?' class='' $aStyle>Settings</a>
                             </li>
                         </ul>
                     </div>
                   </div>";
+        }else if($name == "EmployeesClass"){
+            $link = getLink("EmployeesClass");
+            echo "<div class='collapse multi-collapse' id='multiCollapse_$name'>
+            <div>
+                <ul class='fa-ul'>
+                    <li $liStyle>
+                        $icon
+                        <a href='$link?subMenu=departments' class='' $aStyle>Departments</a>
+                    </li>
+                    <li $liStyle>
+                        $icon
+                        <a href='$link?subMenu=transport_types' class='' $aStyle>Transport Types</a>
+                    </li>
+                </ul>
+            </div>
+          </div>";
         }else{
             echo "<div class='collapse multi-collapse' id='multiCollapse_$name'>
                 <div>
@@ -18,6 +40,11 @@
             </div>";
         }
         
+    }
+
+    function getLink($object){
+        $link = substr($_SERVER['REQUEST_URI'],0,strpos($_SERVER['REQUEST_URI'],"Modules"));
+        return $link."Modules/".$object."/View";
     }
 ?>
 
